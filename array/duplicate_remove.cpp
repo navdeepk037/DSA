1f18ce93fb88c4505a2e7b5627c6b3c8b10d9c82
navdeepk037 <21bcs037@nith.ac.in>
2022-10-18 18:48:44 +0530

    updated array codes

--- array/duplicate_remove.cpp
+++ array/duplicate_remove.cpp
@@ -1,8 +1,26 @@
 #include<iostream>
 using namespace std;
+void display(int a[],int n)
+{
+    for(int i=0;i<n;i++)
+    cout<<a[i]<<" ";
+}
+int duplicate(int a[],int n)
+{
+    int res=0;
+    a[res]=a[0];
+    for(int i=0;i<n-1;i++)
+    {
+        if(a[i+1]!=a[i])
+        a[res++]=a[i+1];
+        
+    }
+    return res;
+}
 int main()
 {
-    int a[]={1,2,2,2,3,3,3,4,4,4,5};
+    int a[]={1,2,3,3,3,4,4,5,5};
     int n=sizeof(a)/sizeof(int);
-    
+    int ans=duplicate(a,n);
+    display(a,ans);
 }
